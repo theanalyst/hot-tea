fcc80a0ec1426b3caa4e843ce984dc5fe2c873c9
Abhishek L <abhishekl.2006@gmail.com>
2014-03-01 22:20:48 +0530

    Adding a basic index template
    * views.py: Rendering a template instead of a raw hello world
    * templates/index.html: Added a basic template
    * static/css/app.css: Basic CSS (not completely responsive)

--- views.py
+++ views.py
@@ -1,7 +1,11 @@
+from flask import render_template
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Hello World!"
+    user = { 'nickname': 'agent47'}
+    return render_template("index.html",
+        title = "47's home",
+        user = user)
 
