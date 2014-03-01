3d1ff7466ca616aacbe541215e671d1a976f2f5a
Abhishek L <abhishekl.2006@gmail.com>
2014-03-02 01:30:45 +0530

    Added functionality for login forms
    However doesn't know receiving form data yet.
    
    * __init__.py: config imported for WTF tokens etc.
    * forms.py: Basic Login form added
    * templates/login.html: Basic login template added
    * views.py: Added routes for login method. Doesn't receive yet

--- views.py
+++ views.py
@@ -1,5 +1,6 @@
-from flask import render_template
+from flask import render_template, flash, redirect
 from app import app
+from forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -20,3 +21,9 @@ def index():
         user = user,
         posts = posts)
 
+@app.route("/login", methods = ['GET', 'POST'])
+def login():
+    form = LoginForm()
+    return render_template("login.html",
+        title = "Sign In",
+        form = form)
