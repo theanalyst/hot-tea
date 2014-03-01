7eeafa990edc47e7bc0be2c34b5e772dae50c2d9
Abhishek L <abhishekl.2006@gmail.com>
2014-03-01 22:57:22 +0530

    Adding a basic posts functionality
    * views.py: Added basic data for posts
    * templates/index.html: Added posts as paragraph attributes
    * static/css/app.css: basic style for paragraph attributes

--- views.py
+++ views.py
@@ -5,7 +5,18 @@ from app import app
 @app.route("/index")
 def index():
     user = { 'nickname': 'agent47'}
+    posts = [
+        {
+            'author' : { 'nickname': "Jon"},
+            'body' : "Some test foo"
+        },
+        {
+            'author' : { 'nickname': "Alice"},
+            'body' : "I am not a kryptographer"
+        }
+    ]
     return render_template("index.html",
         title = "47's home",
-        user = user)
+        user = user,
+        posts = posts)
 
