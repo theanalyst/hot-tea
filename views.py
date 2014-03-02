bc9a56d0db58cc07af84fcc74441e773d6ac9c65
Abhishek L <abhishekl.2006@gmail.com>
2014-03-02 10:41:20 +0530

    Making validation of form on submission
    * views.py: calling validate_on_submit() and flashing the user message

--- views.py
+++ views.py
@@ -1,6 +1,6 @@
 from flask import render_template, flash, redirect
 from app import app
-from forms import LoginForm
+from .forms import LoginForm
 
 @app.route("/")
 @app.route("/index")
@@ -24,6 +24,11 @@ def index():
 @app.route("/login", methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="' + form.openid.data + form.openid.data
+              + '", remember_me="' + str(form.remember_me.data))
+        return redirect("/index")
+
     return render_template("login.html",
         title = "Sign In",
         form = form)
